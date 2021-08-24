0623a603cae8087c62c28fba2f5578e9232c6385
Muhammad Razali <wireless-@hotmail.com>
2021-08-24 22:26:04 +0800

    Added Console Project

---
+++ ObjectComparer/ObjectComparer.Console/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectComparer.Console
+{
+    public class Person
+    {
+        public string Id { get; set; }
+
+        public List<PersonName> PersonNames { get; set; }
+    }
+
+    public class PersonName
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime LastModified { get; set; }
+    }
+
+    public static class PersonFactory
+    {
+        public static List<Person> GetPersons()
+        {
+
+            Person razali = new Person()
+            {
+                Id = "P1",
+                PersonNames = new List<PersonName>()
+                {
+                    new PersonName() { Id = "N1", Name = "Razali", Type = "P", LastModified = DateTime.Now},
+                    new PersonName() { Id = "N2", Name = "Lizara", Type = "A", LastModified = DateTime.Now}
+                }
+            };
+
+            Person john = new Person()
+            {
+                Id = "P2",
+                PersonNames = new List<PersonName>()
+                {
+                    new PersonName() { Id = "N3", Name = "John", Type = "P", LastModified = DateTime.Now},
+                    new PersonName() { Id = "N4", Name = "Johnny", Type = "A", LastModified = DateTime.Now}
+                }
+            };
+
+
+            List<Person> people = new List<Person>() { razali, john};
+
+            return people;
+        }
+    }
+}
---
+++ ObjectComparer/ObjectComparer.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectComparer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = PersonFactory.GetPersons();
+
+            ObjectTracker objectTracker = new ObjectTracker();
+
+            var differences = objectTracker
+                                .Track(people[0].PersonNames, people[1].PersonNames, keyExpression: x => x.Id, x => x.Name, x => x.Type)
+                                .GetDifferences();
+
+            foreach (var difference in differences)
+            {
+                System.Console.WriteLine(difference);
+            }
+        }
+    }
+}
